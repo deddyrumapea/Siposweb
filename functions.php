dcf07299b4bf0942a5fe9e2ef9669372c715b0f2
Deddy Romnan <romnandeddy@gmail.com>
2020-11-13 22:44:43 +0700

    Add more query functions

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 $conn = mysqli_connect("localhost", "root", "", "siposweb");
 
 
-function query($query){
+function queryRead($query){
 	global $conn;
 	$result = mysqli_query($conn, $query);
 	$rows = [];
@@ -12,5 +12,32 @@ function query($query){
 		$rows[] = $row;
 	}
 	return $rows;
+}
+
+function queryCreate($query){
+	global $conn;
+	mysqli_query($conn, $query);
+	if (mysqli_affected_rows($conn) > 0) {
+	 	return true;
+	 } else {
+	 	return false;
+	 }
+}
+
+function queryUpdate($query){
+	global $conn;
+	mysqli_query($conn, $query);
+	if (mysqli_affected_rows($conn) > 0) {
+	 	return true;
+	 } else {
+	 	return false;
+	 }
+}
+
+
+function hapusProduk($id) {
+	global $conn;
+	mysqli_query($conn, "DELETE FROM produk WHERE id = '$id'");
+	return mysqli_affected_rows($conn);
 }
 ?>
